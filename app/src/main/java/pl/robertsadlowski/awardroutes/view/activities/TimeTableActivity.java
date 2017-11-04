e647b71758138ae4c0ba8dd4ebfd930b2478364d
robsad <rob.sadlowski@gmail.com>
2017-11-04 13:45:50 +0100

    return arrow

--- app/src/main/java/pl/robertsadlowski/awardroutes/view/activities/TimeTableActivity.java
+++ app/src/main/java/pl/robertsadlowski/awardroutes/view/activities/TimeTableActivity.java
@@ -3,8 +3,10 @@ package pl.robertsadlowski.awardroutes.view.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -30,6 +32,7 @@ public class TimeTableActivity extends AppCompatActivity {
     private ListView listView;
     private List<TimetableConnection> timetableList = new ArrayList<>();
     private TextView headerText;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +44,19 @@ public class TimeTableActivity extends AppCompatActivity {
         listView.addHeaderView(headerView);
         headerText = (TextView) findViewById(R.id.textTimetableHeader);
         headerText.setText("Waiting...");
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_timetable);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Selected Route Timetable");
+            }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                }
+        });
         Intent intent = getIntent();
         String origin = intent.getStringExtra("Origin");
         String destination = intent.getStringExtra("Destination");
@@ -67,6 +81,7 @@ public class TimeTableActivity extends AppCompatActivity {
                             noResultResponse();
                         } else {
                             listViewDataFeed(html);
+                            toolbar.setSubtitle(getWeekDate());
                         }
                     } catch (IOException e) {
                         Log.d("POST html", e.getMessage());
@@ -114,6 +129,21 @@ public class TimeTableActivity extends AppCompatActivity {
         int thisDay = currentTime.get(Calendar.DAY_OF_MONTH);
         String day = String.format("%02d",thisDay);
         return day + "%2F" + month + "%2F" + thisYear;
+    }
+
+    private String getWeekDate() {
+        Calendar currentTime = Calendar.getInstance();
+        String weekDate;
+        while( currentTime.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY )
+            currentTime.add( Calendar.DATE, 1 );
+        int thisMonth = currentTime.get(Calendar.MONTH)+1;
+        int thisDay = currentTime.get(Calendar.DAY_OF_MONTH);
+        weekDate = "" + thisDay + thisMonth;
+        currentTime.add( Calendar.DATE, 6 );
+        thisMonth = currentTime.get(Calendar.MONTH)+1;
+        thisDay = currentTime.get(Calendar.DAY_OF_MONTH);
+        weekDate = weekDate+thisDay+thisMonth;
+        return weekDate;
     }
 
 }
